c289158eec31f5fcd8a12130e9ee1dded8735040
Daniel Mendez <danielsaso@gmail.com>
2022-04-18 21:38:37 +0200

    livecode

---
+++ parsing/livecode/spec/scraper_spec.rb
@@ -0,0 +1,18 @@
+data for #fetch_movies_url:
+[
+  "https://www.imdb.com/title/tt0111161/",
+  "https://www.imdb.com/title/tt0068646/",
+  "https://www.imdb.com/title/tt0071562/",
+  "https://www.imdb.com/title/tt0468569/",
+  "https://www.imdb.com/title/tt0050083/"
+]
+
+data for #scrape_movie:
+{
+  cast: [ "Christian Bale", "Heath Ledger", "Aaron Eckhart" ],
+  director: "Christopher Nolan",
+  storyline: "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+  title: "The Dark Knight",
+  year: 2008
+}
+
